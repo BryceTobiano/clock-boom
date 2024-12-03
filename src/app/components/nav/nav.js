e100ef145acb6db0a5352f5d607c332e8f49a175
primprrim <boonyach@usc.edu>
2024-12-03 00:07:23 -0800

    updated navbar interactivity

--- src/app/components/nav/nav.js
+++ src/app/components/nav/nav.js
@@ -3,13 +3,16 @@
 import { FaSearch, FaCalendar, FaTh, FaChartBar, FaUserCircle } from 'react-icons/fa';
 import Link from 'next/link';
 import Image from 'next/image'; 
+import { usePathname } from 'next/navigation';
 import styles from './nav.module.css';
 
 export default function Navbar() {
-  const iconStyle = {
-    color: 'grey',
+  const pathname = usePathname();
+  
+  const getIconStyle = (path) => ({
+    color: pathname === path ? '#45B681' : '#C0C0C0',
     transition: 'color 0.3s ease',
-  };
+  });
 
   return (
     <div className={styles.navbar}>
@@ -17,33 +20,33 @@ export default function Navbar() {
         <Link className={styles.navbar_item} href="/dashboard">
           <FaTh
             size={30}
-            style={iconStyle}
+            style={getIconStyle('/dashboard')}
             onMouseEnter={(e) => (e.target.style.color = '#45B681')}
-            onMouseLeave={(e) => (e.target.style.color = 'grey')}
+            onMouseLeave={(e) => (e.target.style.color = pathname === '/dashboard' ? '#45B681' : '#C0C0C0')}
           />
         </Link>
         <Link className={styles.navbar_item} href="/calendar">
           <FaCalendar
             size={30}
-            style={iconStyle}
+            style={getIconStyle('/calendar')}
             onMouseEnter={(e) => (e.target.style.color = '#45B681')}
-            onMouseLeave={(e) => (e.target.style.color = 'grey')}
+            onMouseLeave={(e) => (e.target.style.color = pathname === '/calendar' ? '#45B681' : '#C0C0C0')}
           />
         </Link>
         <Link className={styles.navbar_item} href="/activity_analysis">
           <FaChartBar
             size={30}
-            style={iconStyle}
+            style={getIconStyle('/activity_analysis')}
             onMouseEnter={(e) => (e.target.style.color = '#45B681')}
-            onMouseLeave={(e) => (e.target.style.color = 'grey')}
+            onMouseLeave={(e) => (e.target.style.color = pathname === '/activity_analysis' ? '#45B681' : '#C0C0C0')}
           />
         </Link>
         <Link className={styles.navbar_item} href="/time_finder">
           <FaSearch
             size={30}
-            style={iconStyle}
+            style={getIconStyle('/time_finder')}
             onMouseEnter={(e) => (e.target.style.color = '#45B681')}
-            onMouseLeave={(e) => (e.target.style.color = 'grey')}
+            onMouseLeave={(e) => (e.target.style.color = pathname === '/time_finder' ? '#45B681' : '#C0C0C0')}
           />
         </Link>
       </div>
